e6c04f692f418d78b0cbbb1ed0663250e7f1385e
Dharyel Honório <dharyel@publi.com.br>
2025-07-29 07:11:34 -0300

    chore: initial setup

---
+++ src/main/java/com/oldschoolsolucoestech/desafio2_java_professional/Desafio2JavaProfessionalApplication.java
@@ -0,0 +1,13 @@
+package com.oldschoolsolucoestech.desafio2_java_professional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desafio2JavaProfessionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Desafio2JavaProfessionalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oldschoolsolucoestech/desafio2_java_professional/Desafio2JavaProfessionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oldschoolsolucoestech.desafio2_java_professional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desafio2JavaProfessionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
